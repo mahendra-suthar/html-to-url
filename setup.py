e8e41ef498234403e4d65a12eab0f1ef5f8007cf
Mahendra Suthar <mksuthar9016@gmail.com>
2021-09-04 06:20:23 -0700

    htmlToZip==0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="htmlToZip",
-    version=0.1,
+    version=0.2,
     author="Mahendra kumar",
     author_email="mksuthar9016@gmail.com",
     description="Convert your html, css and js code into project",
